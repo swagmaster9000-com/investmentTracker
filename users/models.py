e72e28074d8f937b8630208cc18a23c53da16ce5
Michelle <ampride@memphis.edu>
2025-11-11 21:25:02 -0600

    Add the model class for a custom user setup

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
